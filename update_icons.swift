ecff144880e606c164e1b4849e38f8242e706670
Maicol Orejuela <viafara96@hotmail.com>
2025-04-26 13:53:22 -0500

    add new icons for apps
    add github icon and vs code

--- update_icons.swift
+++ update_icons.swift
@@ -6,6 +6,8 @@ on run
 	set spotifyIconPath to userHome & "/Documents/icons/spotify.icns"
 	set slackIconPath to userHome & "/Documents/icons/electron.icns"
 	set warpIconPath to userHome & "/Documents/icons/Warp.icns"
+	 set vscodeIconPath to userHome & "Code.icns"
+    set githubIconPath to userHome & "github.icns"
 	
 	-- Spotify (Example application on an external disk)
 	set spotifyAppPath to "/Volumes/ExternalDisk/Spotify/Spotify.app"
@@ -16,6 +18,14 @@ on run
 	set slackAppPath to "/Applications/Slack.app"
 	set slackAppIconPath to slackAppPath & "/Contents/Resources/electron.icns"
 	
+	    
+	-- GitHub Desktop
+    set githubAppPath to "/Applications/GitHub Desktop.app"
+    set githubAppIconPath to githubAppPath & "/Contents/Resources/electron.icns"
+    
+    -- VS Code
+    set vscodeAppPath to "/Applications/Visual Studio Code.app"
+    set vscodeAppIconPath to vscodeAppPath & "/Contents/Resources/Code.icns"
 	-- Warp (Example with an icon in multiple locations)
 	set warpAppPath to "/Applications/Warp.app"
 	set warpAppIconPath to warpAppPath & "/Contents/Resources/Warp.icns"
@@ -24,6 +34,8 @@ on run
 	-- Replace icons inside the application bundles
 	do shell script "cp -f " & quoted form of spotifyIconPath & " " & quoted form of spotifyAppIconPath
 	do shell script "cp -f " & quoted form of slackIconPath & " " & quoted form of slackAppIconPath
+	do shell script "cp -f " & quoted form of githubIconPath & " " & quoted form of githubAppIconPath
+    do shell script "cp -f " & quoted form of vscodeIconPath & " " & quoted form of vscodeAppIconPath
 	do shell script "cp -f " & quoted form of warpIconPath & " " & quoted form of warpAppIconPath
 	do shell script "rm -f " & quoted form of warpDockIconPath
 	do shell script "cp -f " & quoted form of warpIconPath & " " & quoted form of warpDockIconPath
@@ -42,11 +54,21 @@ on run
 			set slackIconRef to POSIX file slackIconPath as alias
 			set slackAppRef to POSIX file slackAppPath as alias
 			set icon of slackAppRef to slackIconRef
-			
+            -- GitHub Desktop
+            set githubIconRef to POSIX file githubIconPath as alias
+            set githubAppRef to POSIX file githubAppPath as alias
+            set icon of githubAppRef to githubIconRef
+            
+            -- VS Code
+            set vscodeIconRef to POSIX file vscodeIconPath as alias
+            set vscodeAppRef to POSIX file vscodeAppPath as alias
+            set icon of vscodeAppRef to vscodeIconRef
+
+
 			-- Warp
 			set warpIconRef to POSIX file warpIconPath as alias
 			set warpAppRef to POSIX file warpAppPath as alias
-			set icon of warpAppRef to warpIconRef
+			set icon of warpIconRef to warpAppRef
 		end try
 	end tell
 	
